b366d4a0c893448aed1188320328ef6f132b9434
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-10-08 18:45:40 +0100

    fix popsquare import from public folder

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,11 @@
 import "./Header.css";
-// import logo from "%PUBLIC_URL%/logo192.png";
-import popsquare from "/images/popsquare.svg";
 import Button from "../Button/Button";
 
 export default function Header() {
 	return (
 		<header className="Header" id="header">
 			<div id="pop-square">
-				<img src="/images/popsquare.svg" alt="" />
+				<img src="./images/popsquare.svg" alt="" />
 			</div>
 			<div id="title-name">
 				<h1>Hannah McFarlane</h1>
