8a3f93ad4170bbacba64004a1b4e3244ae38584e
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-10-08 19:08:16 +0100

    add info section (Bio component)

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,35 +1,47 @@
+import "./Bio.css";
+import Button from "../Button/Button";
+
 export default function Bio() {
 	return (
 		<section id="info-sect">
-			<div class="main-sect" id="code-cont">
+			<div className="main-sect" id="code-cont">
 				<h2 id="h2-code">Coding</h2>
-				<div class="main-code-wrapper">
-					<div class="text-wrapper-code">
-						<p class="text">
+				<div className="main-code-wrapper">
+					<div className="text-wrapper-code">
+						<p className="text">
 							Lorem ipsum dolor sit amet consectetur adipisicing
 							elit. Nesciunt sint, cupiditate deserunt, magni
 							rerum delectus qui nemo recusandae doloribus
 							blanditiis veritatis distinctio fugiat totam,
 							repellat repudiandae!
 						</p>
-						<p class="text">
+						<p className="text">
 							Lorem ipsum dolor sit amet consectetur adipisicing
 							elit. Nesciunt sint, cupiditate deserunt, magni
 							rerum delectus qui nemo recusandae doloribus.
 						</p>
-						<p class="text">
-							<button class="button-blue github">GitHub</button>
-							<button class="button-pink linkedin">
-								LinkedIn
-							</button>
+						<p className="text">
+							<Button
+								colour="blue"
+								addClass="github"
+								text="GitHub"
+								link="https://github.com/hcmcfarlane"
+								target="_blank"
+							/>
+							<Button
+								colour="pink"
+								addClass="linkedin"
+								text="LinkedIn"
+								link="https://www.linkedin.com/in/hannah-mcfarlane-8a764777/"
+								target="_blank"
+							/>
 						</p>
 					</div>
-					<div class="img-wrapper">
-						<div class="bg-img-cont"></div>
+					<div className="img-bang-wrapper">
+						{/* <div className="bg-img-cont"></div> */}
 						<img
-							class="img-deco"
 							id="img-bang"
-							src="./comic-bang-vintage-style-vintage-comic-style-vector-bang-104334659-TOREPLACE.jpg"
+							src="/images/comic-bang-vintage-style-vintage-comic-style-vector-bang-104334659-TOREPLACE.jpg"
 							alt=""
 						/>
 					</div>
