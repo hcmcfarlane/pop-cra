460f2c58cdc9a78d3501ef014f9827fba9ae7ee9
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-10-08 19:03:30 +0100

    add defaults & error handling for button props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,19 @@
 import "./Button.css";
 
 export default function Button({ colour, text, link, target }) {
-	let classColour = "button-" + colour;
+	let classColour;
 	let linkTarget;
+	let linkLink;
+	let buttonText;
+	colour ? (classColour = "button-" + colour) : (classColour = "button-blue");
 	target ? (linkTarget = target) : (linkTarget = "");
+	link ? (linkLink = link) : (linkLink = "");
+	text ? (buttonText = text) : (buttonText = "");
 
 	return (
 		<button className={classColour}>
-			<a href={`${link}`} target={linkTarget}>
-				{text}
+			<a href={`${linkLink}`} target={linkTarget}>
+				{buttonText}
 			</a>
 		</button>
 	);
