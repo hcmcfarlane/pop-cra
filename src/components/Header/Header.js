29accf5c7fe426a9f25ae308a44b529b92951108
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-10-08 18:59:59 +0100

    change link target to Home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,20 +3,26 @@ import Button from "../Button/Button";
 
 export default function Header() {
 	return (
-		<header className="Header" id="header">
-			<div id="pop-square">
-				<img src="./images/popsquare.svg" alt="" />
-			</div>
-			<div id="title-name">
-				<h1>Hannah McFarlane</h1>
-			</div>
-			<div id="menu-flex-cont">
-				<div id="menu-buttons-cont">
-					<Button colour="pink" link="#header" text="Home" />
-					<Button colour="yellow" link="#projects" text="Projects" />
-					<Button colour="blue" link="#contact" text="Contact" />
+		<div id="home">
+			<header className="Header" id="header">
+				<div id="pop-square">
+					<img src="./images/popsquare.svg" alt="" />
 				</div>
-			</div>
-		</header>
+				<div id="title-name">
+					<h1>Hannah McFarlane</h1>
+				</div>
+				<div id="menu-flex-cont">
+					<div id="menu-buttons-cont">
+						<Button colour="pink" link="#header" text="Home" />
+						<Button
+							colour="yellow"
+							link="#projects"
+							text="Projects"
+						/>
+						<Button colour="blue" link="#contact" text="Contact" />
+					</div>
+				</div>
+			</header>
+		</div>
 	);
 }
