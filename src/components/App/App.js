2c3bb38b7b7b782048109bc195b991abc5f1363a
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-10-08 18:38:36 +0100

    remove CRA template and import Header and Bio components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "%PUBLIC_URL%/logo192.png";
+import Bio from "../Bio/Bio";
+import Header from "../Header/Header";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id="App">
+			<Header />
+			<main>
+				<Bio />
+			</main>
+			<footer></footer>
+		</div>
+	);
 }
 
 export default App;
