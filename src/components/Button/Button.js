a58cb2fb38dbea87805cd847ec5e61f1ce878309
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-10-08 19:07:12 +0100

    add additional class options to Button component via props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,13 @@
 import "./Button.css";
 
-export default function Button({ colour, text, link, target }) {
+export default function Button({ colour, text, link, target, addClass }) {
 	let classColour;
 	let linkTarget;
 	let linkLink;
 	let buttonText;
-	colour ? (classColour = "button-" + colour) : (classColour = "button-blue");
+	colour
+		? (classColour = `button-${colour} ${addClass}`)
+		: (classColour = `button-blue ${addClass}`);
 	target ? (linkTarget = target) : (linkTarget = "");
 	link ? (linkLink = link) : (linkLink = "");
 	text ? (buttonText = text) : (buttonText = "");
