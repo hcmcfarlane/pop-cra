d1f9477c464a469c5e61160a47ce6674b08db5f4
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-10-08 18:39:41 +0100

    start Bio component

---
+++ src/components/Bio/Bio.js
@@ -0,0 +1,40 @@
+export default function Bio() {
+	return (
+		<section id="info-sect">
+			<div class="main-sect" id="code-cont">
+				<h2 id="h2-code">Coding</h2>
+				<div class="main-code-wrapper">
+					<div class="text-wrapper-code">
+						<p class="text">
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Nesciunt sint, cupiditate deserunt, magni
+							rerum delectus qui nemo recusandae doloribus
+							blanditiis veritatis distinctio fugiat totam,
+							repellat repudiandae!
+						</p>
+						<p class="text">
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Nesciunt sint, cupiditate deserunt, magni
+							rerum delectus qui nemo recusandae doloribus.
+						</p>
+						<p class="text">
+							<button class="button-blue github">GitHub</button>
+							<button class="button-pink linkedin">
+								LinkedIn
+							</button>
+						</p>
+					</div>
+					<div class="img-wrapper">
+						<div class="bg-img-cont"></div>
+						<img
+							class="img-deco"
+							id="img-bang"
+							src="./comic-bang-vintage-style-vintage-comic-style-vector-bang-104334659-TOREPLACE.jpg"
+							alt=""
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
